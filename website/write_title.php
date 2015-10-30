0ffb0a118556f2f2675530866ea3ff804acb0103
Eoin Carley <eoincarley@gmail.com>
2015-10-30 09:25:01 +0000

    write_title.php edited online with Bitbucket

--- website/write_title.php
+++ website/write_title.php
@@ -84,9 +84,8 @@
 			//$human_readable_date_line = "$human_readable_date";
 		//	Write out and close the rest of the table
 		
-		if ($this_page == 'forecast.php'){ //&& $this_page == 'forecast.php'){strtotime($date) <= strtotime("2050831")
+		if ($this_page == 'forecast.php'){ //&& $this_page == 'forecast.php'){strtotime($date) <= strtotime("20150831")
 			$this_page = "forecast_no_fps.php";
-			echo 'HERE!!!!!';
 		}
 		echo $this_page;
 		
