a7dcc0004cebe228792f100cc99b567d6a15a2f7
Aoife McCloskey <mccloska@tcd.ie>
2015-11-04 18:52:03 +0000

    Fixed minor bug to include no flares for day previous

--- website/functions.php
+++ website/functions.php
@@ -539,36 +539,77 @@ function link_image($file, $size, $map)
 	{
 		$ev_str = '' ;
 		$y = 0 ;
+		$y_events=0 ;
 
 		if($events[0] != '-')
 		{
 			$events_arr = split('[ ]' , $events) ;
+			$n_events = count($events_arr) ;
+			
+			if ($events_arr[0] != '/')
+			{
+				$t_events=1;
+			}
+			else
+			{
+				$t_events=0;
+			}
+			
 
-					for ($i = 0 ; $i < count($events_arr) ; ++$i)
+			for ($i = 0 ; $i < count($events_arr) ; ++$i)
 			{
 				if ($events_arr[$i] == "/")	
 				{
 					$y = 1 ;
+					$y_events=1;
 				}
 				else
 				{
 					$url[] = $events_arr[$i];
 					$data[] = $events_arr[$i+1];
 					list($size,$time) = split('[(]', $events_arr[$i+1] , 2);
-			
+					
 					if ($y == 1) 
 					{ 
 		
 						$ev_time[] = (strtotime(substr($time,0,5)) - strtotime("00:00"))/60. - 1440 ;  //time in minutes from midnight.
 						$time_inst = (strtotime(substr($time,0,5)) - strtotime("00:00"))/60. - 1440 ;  //time in minutes from midnight.
 						$col = "#58ACFA";
+						if ($i == ($n_events-2))
+						switch ($t_events){
+						case 0:
+						{
+						$data[count($data)-1] = "<font color=\"black\">- / </font>" . $data[count($data)-1] ;
+						break;
+						}
+						case 1:
+						{
+						$data[count($data)-1] = "<font color=\"black\">/ </font>" . $data[count($data)-1] ;
+						break;
+						}
+						}
 					}
 					else
 					{
 						$ev_time[] = (strtotime(substr($time,0,5)) -strtotime("00:00"))/60.;  //time in minutes from midnight.
 						$time_inst =  (strtotime(substr($time,0,5)) -strtotime("00:00"))/60.;  //time in minutes from midnight.
 						$col = "#0000FF" ;
-					}
+						if ($i == 0)
+						{
+							switch ($y_events){
+							case 0:
+							{
+							$data[count($data)-1] =  $data[count($data)-1] ;
+							break;
+							}
+							case 1:
+							{
+							$data[count($data)-1] =  $data[count($data)-1]."<font color=\"black\"> / -  </font>" ;
+							break;
+							}
+							}
+						}
+					}						
 
 					$url_inst = $events_arr[$i] ;
 					$data_inst = $data[count($data)-1] ;
@@ -592,6 +633,7 @@ function link_image($file, $size, $map)
 								continue ;
 							}
 					}
+					$y_events=0;
 					++$i ;
 				}
 			}
